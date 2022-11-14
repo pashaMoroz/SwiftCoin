0b8b7983728083a7f560913aeba88ada5747e5c6
Moroz Pavlo <vferme2015@gmail.com>
2022-11-14 10:38:49 -0500

    Initial Commit

---
+++ SwiftCoin/SwiftCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCoinApp.swift
+//  SwiftCoin
+//
+//  Created by Admin on 14/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
